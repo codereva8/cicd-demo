d6d931afbc11975e713d12c7c2cf55fccf560836
codereva8 <patilrevarajesh@gmail.com>
2026-04-13 12:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello CI/CD")
-print("Hello AAAAhhhhhhhhhhhhAAAAAAI/CD")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from CI/CD 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
